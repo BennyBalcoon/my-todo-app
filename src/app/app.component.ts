bc48e2d7c8d655dc61fabb4cd9bf4d32bf57de84
BennyBalcoon <benoitbalcon@hotmail.fr>
2019-07-04 18:41:32 +0200

    add project files

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-todo-app';
+  todoTask = [
+    'Repasser mes chaussettes',
+    'Faire la vaisselle'
+  ];
+
+  wipTask = [
+    'Passer chez Sosh',
+    'fucking angular'
+  ];
+
+  doneTask = [
+    'Manger des pommes',
+    'boire boire boire'
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+}
+
